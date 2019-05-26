07a2f951f59fdf927398da5d97d4f8f8393eb694
Peter Vashevko <peter@vashevko.com>
2019-05-25 22:52:56 -0400

    Big change

--- index.js
+++ index.js
@@ -1,87 +1,99 @@
 const express = require('express');
-const CookieParser = require('cookie-parser');
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const session = require('express-session');
+const fs = require('fs');
 const utils = require('./utils');
 const path = require('path');
 const app = express();
 const port = 5849;
 
-const url = 'mongodb://localhost:27017/environment-userdb';
+let users = {};
+let google_to_id = {};
+
+users = JSON.parse(fs.readFileSync('climateconnections_users.json'));
+google_to_id = JSON.parse(fs.readFileSync('climateconnections_googletoid.json'));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('frontend'));
-app.use(CookieParser('9442b610-c141-4de5-9928-f1cc63dbdc72')); // Cookie Secret (SECRET)
-
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
+app.use(session({
+    secret: '9442b610-c141-4de5-9928-f1cc63dbdc72', // Cookie Secret (SECRET)
+    cookie: {
+        path: '/',
+        maxAge: 1000 * 60 * 24,
+    }
+}));
     
-    app.get('/', (req, res) => {
-        res.redirect('/sign-in');
-    });
-    
-    app.post('/report-powerstrip', (req, res) => {
-        const json = res.json(req.body);
-        console.log(json);
-    });
-
-    app.get('/sign-in', (req, res) => {
-        res.sendFile(path.resolve('frontend/sign.html'));
-    });
-
-    app.post('/verify-user', (req, res) => {
-        console.log('fjaskl');
-        const json = req;
-        console.log(json);
-        let user;
-        if (json.google_token) {
-            console.log('faaff');
-            user = utils.verifyUser(db, json.google_token);
-            console.log('mem', user);
-            if (!user) {
-                user = utils.addUser(db, google_token);
-            }
-            console.log('mem2', user);
-        }
-        if (user) {
-            console.log('dass');
-            res.cookie('token', user.token, { path: '/', secure: true });
-            res.cookie('id', user.id, { path: '/', secure: true });
-            res.redirect('/profile');
-        } else {
-            console.log('fdjs');
-            res.sendStatus(500);
-        }
-    });
-
-    app.get('/profile', (req, res) => {
-        const userId = req.signedCookies.id;
-        const userToken = req.signedCookies.token;
-        if (userId && userToken) {
-            const userData = utils.getUserData(db, userId, userToken);
-            if (userData) {
-                res.render(
-                    path.resolve('./frontend/profile.ejs'),
-                    {
-                        user: userData,
-                    });
-            } else {
-                res.redirect('/sign-in');
-            }
-        } else {
-            res.redirect('/sign-in');
-        }
-    });
-
-    app.listen(port, () => console.log(`Server running on port ${port}...`));
-
-    // Exit handlers
-    process.on('exit', db.close);
-    process.on('SIGINT', db.close);
-    process.on('SIGUSR1', db.close);
-    process.on('SIGUSR2', db.close);
-    process.on('uncaughtException', db.close);
+app.get('/', (req, res) => {
+    res.redirect('/sign-in');
 });
+
+app.post('/report-powerstrip', (req, res) => {
+    const json = req.body;
+    console.log(json);
+    res.sendStatus(200);
+});
+
+app.get('/sign-in', (req, res) => {
+    res.sendFile(path.resolve('frontend/sign.html'));
+});
+
+app.post('/verify-user', (req, res) => {
+    const json = req.body;
+    if (json.google_token) {
+        const id = google_to_id[json.google_token];
+        if (id) {
+            const user = users[id];
+            if (user) {
+                req.session.token = user.token;
+                req.session.uid = user.id;
+                res.sendStatus(200);
+                console.log('found');
+            }
+        } else {
+            const newUId = utils.generateToken();
+            const newUserToken = utils.generateToken();
+            google_to_id[json.google_token] = newUId;
+            users[newUId] = {
+                id: newUId,
+                google_token: json.google_token,
+                token: newUserToken,
+                level: 1,
+                points: 0,
+                levelpoints: 0,
+                friends: {},
+            };
+            fs.writeFile('climateconnections_users.json', JSON.stringify(users), (err) => {
+                if (err) throw err;
+                console.log('Users Saved');
+            });
+            fs.writeFile('climateconnections_googletoid.json', JSON.stringify(google_to_id), (err) => {
+                if (err) throw err;
+                console.log('ID Saved');
+            });
+            req.session.token = newUserToken;
+            req.session.uid = newUId;
+            res.sendStatus(200);
+        }
+    }
+});
+
+app.get('/profile', (req, res) => {
+    console.log('1');
+    const userId = req.session.uid;
+    const userToken = req.session.token;
+    if (userId && userToken) {
+        console.log('2');
+        const userData = users[userId];
+        if (userData.token == userToken) {
+            console.log('3');
+            res.render(path.resolve('./frontend/profile.ejs'),
+                {
+                    user: userData,
+                });
+        }
+    } else {
+        res.location('/sign-in');
+    }
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}...`));
--- utils.js
+++ utils.js
@@ -1,45 +1,7 @@
 const uuidv4 = require('uuid/v4');
 
-const addUser = (db, id) => {
-    const collection = db.collection('users');
-    collection.insertOne({
-        id: generateToken(),
-        google_token: id,
-        token: generateToken(),
-        friends: {},
-    }, function(err, result) {
-        if (err) {
-            return;
-        } else {
-            return { token: result.ops[0].token, id: result.ops[0].id };
-        }
-    });
-};
-
-const verifyUser = (db, id) => {
-    console.log('fsa');
-    const collection = db.collection('users');
-    const userFile = collection.findOne({ google_token: id });
-    if (userFile) {
-        return { token: userFile.token, id: userFile.id };
-    } else {
-        return;
-    }
-};
-
-const getUserData = (db, id, token) => {
-    const collection = db.collection('users');
-    const userFile = collection.findOne({ google_token: id });
-    if (userFile) {
-        return { token: userFile.token, id: userFile.id };
-    } else {
-        return;
-    }
-};
-
 const generateToken = () => uuidv4();
 
 module.exports = {
-    addUser,
-    verifyUser,
+    generateToken,
 };
