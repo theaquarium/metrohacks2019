2f85316a4713250ad6d7ff1f38e763afa349da57
Peter Vashevko <peter@vashevko.com>
2019-05-26 04:47:50 -0400

    fdsas

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ const port = 5849;
 let users = {};
 let google_to_id = {};
 let events = [];
+let hourlyPower = [];
+let powerstripCounter = 0;
 
 users = JSON.parse(fs.readFileSync('climateconnections_users.json'));
 google_to_id = JSON.parse(fs.readFileSync('climateconnections_googletoid.json'));
 events = JSON.parse(fs.readFileSync('climateconnections_events.json'));
+hourlyPower = JSON.parse(fs.readFileSync('climateconnections_hourlypower.json'));
 
 app.use(express.json());
 app.set('view engine', 'ejs');
@@ -37,6 +40,7 @@ app.post('/report-powerstrip', (req, res) => {
             const event = {
                 "event_type": "powerstrip",
                 "current": json.current,
+                "timestamp": Date.now(),
                 "id": json.id,
             };
             events.push(event);
@@ -45,6 +49,17 @@ app.post('/report-powerstrip', (req, res) => {
                 console.log('Events Saved');
             });
             res.sendStatus(200);
+            powerstripCounter++;
+            if (powerstripCounter == 360) {
+                hourlyPower.push(event);
+                fs.writeFile('climateconnections_hourlypower.json', JSON.stringify(hourlyPower), (err) => {
+                    if (err) throw err;
+                    console.log('Hourly Saved');
+                });
+                const newUser = utils.addPowerStripPoints(hourlyPower, users[json.id], json.current);
+                users[json.id] = newUser;
+                powerstripCounter = 0;
+            }
         }
     }
 });
@@ -113,38 +128,47 @@ app.get('/profile', (req, res) => {
 });
 
 app.get('/dashboard', (req, res) => {
-    const userId = req.session.uid;
+    /* const userId = req.session.uid;
     const userToken = req.session.token;
     if (userId && userToken) {
         const userData = users[userId];
         if (userData.token == userToken) {
-            res.render(path.resolve('./frontend/profile.ejs'),
+            res.render(path.resolve('./frontend/dashboard.html'),
                 {
                     user: userData,
                 });
         }
     } else {
         res.location('/sign-in');
-    }
+    } */
+    res.sendFile(path.resolve('./frontend/dash.html'));
 });
 
 app.get('/report-compostedtoday', (req, res) => {
+    console.log('421');
     const userId = req.session.uid;
     const userToken = req.session.token;
+    console.log(userId, userToken);
     if (userId && userToken) {
+        console.log('1');
         const userData = users[userId];
         if (userData.token == userToken) {
+            console.log('2');
             if (utils.checkForEventOnDate) {
                 const event = {
                     "event_type": "compostedtoday",
                     "date": new Date().toISOString().substring(0, 10),
                     "id": userId,
                 };
+                console.log('hi');
                 events.push(event);
                 fs.writeFile('climateconnections_events.json', JSON.stringify(events), (err) => {
                     if (err) throw err;
                     console.log('Events Saved');
                 });
+                const newUser = utils.addCompostPoints(userData);
+                console.log(newUser);
+                users[userId] = newUser;
                 res.sendStatus(200);
             } else {
                 res.sendStatus(500);
@@ -172,6 +196,8 @@ app.get('/report-zerowasteday', (req, res) => {
                     if (err) throw err;
                     console.log('Events Saved');
                 });
+                const newUser = utils.addZeroWastePoints(userData);
+                users[userId] = newUser;
                 res.sendStatus(200);
             } else {
                 res.sendStatus(500);
--- utils.js
+++ utils.js
@@ -11,7 +11,66 @@ const checkForEventOnDate = (events, type, date) => {
     }
     return true;
 };
+
+const addPowerStripPoints = (events, user, now) => {
+    let recentPowerStrip;
+    let powerEvents;
+    events.forEach(event => {
+        if (event.type == "powerstrip" && event.id == user.id) {
+            powerEvents.push(event);
+        }
+    });
+    powerEvents.forEach(function(event) {
+        if ((Date.now() - event.timestamp) < 1500000) {
+            recentPowerStrip.push(event.current);
+        }
+    });
+    let sum = 0;
+    recentPowerStrip.forEach(function(current) {
+        sum += current;
+    });
+    const avg = sum / recentPowerStrip.length;
+    const diff = avg - now;
+    if (diff > 0) {
+        const points = diff*1000;
+        user.points += points;
+        user.levelpoints += points;
+        if (user.levelpoints >= 50*2^(user.level)) {
+            user.levelpoints -= 50*2^(user.level);
+            user.level++;
+        }
+    }
+    return user;
+};
+
+const addCompostPoints = (user) => {
+    const points = 100;
+
+    user.points += points;
+    user.levelpoints += points;
+    if (user.levelpoints >= 50*2^(user.level)) {
+        user.levelpoints -= 50*2^(user.level);
+        user.level++;
+    }
+    return user;
+};
+
+const addZeroWastePoints = (user) => {
+    const points = 250;
+
+    user.points += points;
+    user.levelpoints += points;
+    if (user.levelpoints >= 50*2^(user.level)) {
+        user.levelpoints -= 50*2^(user.level);
+        user.level++;
+    }
+    return user;
+};
+
 module.exports = {
     generateToken,
     checkForEventOnDate,
+    addCompostPoints,
+    addPowerStripPoints,
+    addZeroWastePoints,
 };
